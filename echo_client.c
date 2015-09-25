c71c30c66cccefdc7d2ed7bb9381b3d168f5ee11
muupu <once645@gmail.com>
2015-09-25 17:23:31 +0800

    add comments

--- echo_client.c
+++ echo_client.c
@@ -11,14 +11,16 @@ static int port;
 static char addr[256];
 static int nlen;
 static char msg[]="hello\n";
+
+// "./client localhost 25341"
 int main(int argc, char*argv[])
 {
     
     int fd;
-    struct sockaddr_in sin;
+    struct sockaddr_in sin; // Socket套接字地址(参考http://blog.sina.com.cn/s/blog_6444798b0100n529.html)
     
     memset(&sin, 0, sizeof(sin));
-    sin.sin_family = AF_INET;
+    sin.sin_family = AF_INET; // 协议簇为（TCP/IP协议传输 – IPv4）
   
     if(argc<3)
     {
@@ -28,15 +30,15 @@ int main(int argc, char*argv[])
         port = atoi(argv[2]);
     }
     
-    sin.sin_port = htons(port);
+    sin.sin_port = htons(port); // 端口号(htons()将一个数的高低位互换,将一个16位数从主机字节顺序转换成网络字节顺序)
     
     if(argc<2)
     {
         strcpy(addr, argv[1]);
-        if (inet_pton(AF_INET, addr, &(sin.sin_addr) )<0)
+        if (inet_pton(AF_INET, addr, &(sin.sin_addr) )<0) // inet_pton转换字符串到in_addr的结构体, 32位的IP地址
         {
              struct hostent *psh;  
-             psh=gethostbyname(addr);
+             psh=gethostbyname(addr); // 用域名或主机名获取IP地址
              if(psh!=NULL)
                 inet_pton(AF_INET, psh->h_addr, &(sin.sin_addr) );
              else
@@ -44,16 +46,16 @@ int main(int argc, char*argv[])
         }
     }
     
-    if((fd=socket(AF_INET, SOCK_STREAM, 0))<0)
+    if((fd=socket(AF_INET, SOCK_STREAM, 0))<0) //当套接字创建成功时，返回套接字。protocol一般设置为“0”
         cerror("socket");
     
-    if(connect(fd, (struct sockaddr*)&sin, sizeof(sin))<0 )
+    if(connect(fd, (struct sockaddr*)&sin, sizeof(sin))<0 ) // TCP建立连接
         cerror("sonnect");
     
-    if( (nlen = write(fd,msg, strlen(msg)))<0 )
+    if( (nlen = write(fd,msg, strlen(msg)))<0 )  // 发送“hello\n”
         cerror("write");
         
-    if( (nlen = read(fd,msg, strlen(msg)))<0 )
+    if( (nlen = read(fd,msg, strlen(msg)))<0 )  // 服务器回应信息
         cerror("read");
     msg[nlen]='\0';
     printf("msg: %s\n", msg);
--- ser.c
+++ ser.c
@@ -69,26 +69,32 @@ void on_accept(int sock, short event, void* arg)
     event_add(ev->read_ev, NULL);
 }
 
+// 参考http://www.cnblogs.com/cnspace/archive/2011/07/19/2110891.html
 int main(int argc, char* argv[])
 {
     struct sockaddr_in my_addr;
     int sock;
 
-    sock = socket(AF_INET, SOCK_STREAM, 0);
+    sock = socket(AF_INET, SOCK_STREAM, 0); // 1. socket
     int yes = 1;
-    setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
+    setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)); // SO_REUSEADDR是让端口释放后立即就可以被再次使用
     memset(&my_addr, 0, sizeof(my_addr));
     my_addr.sin_family = AF_INET;
     my_addr.sin_port = htons(PORT);
     my_addr.sin_addr.s_addr = INADDR_ANY;
-    bind(sock, (struct sockaddr*)&my_addr, sizeof(struct sockaddr));
-    listen(sock, BACKLOG);
+    bind(sock, (struct sockaddr*)&my_addr, sizeof(struct sockaddr)); // 2. bind
+    listen(sock, BACKLOG);  // 3. listen
 
-    struct event listen_ev;
-    base = event_base_new();
+    struct event listen_ev; 
+    base = event_base_new(); //创建一个事件处理的全局变量,处理IO的管家
+    // 在listen_ev这个事件监听sock这个描述字的读操作，当读消息到达是调用on_accept函数
     event_set(&listen_ev, sock, EV_READ|EV_PERSIST, on_accept, NULL);
+    // 将listen_ev注册到base，相当于告诉处理IO的管家请留意我的listen_ev上的事件
     event_base_set(base, &listen_ev);
+    // 告诉处理IO的管家，当有我的事件到达时你发给我(调用on_accept函数)，至此对listen_ev的初始化完毕
     event_add(&listen_ev, NULL);
+
+    // 正式启动libevent的事件处理机制，使系统运行起来
     event_base_dispatch(base);
 
     return 0;
