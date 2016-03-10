e4ec88f7e83bfb58479e95d461619c315d599b49
muupu <once645@gmail.com>
2016-03-10 08:51:48 +0800

    PF_UNIX tcp_server

--- tcp_server.c
+++ tcp_server.c
@@ -68,11 +68,11 @@ int main(int argc, char *argv[])
     /* daveti: display our pid */
     printf("TCP server pid [%u]\n", getpid());
 
-    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    listenfd = socket(PF_UNIX, SOCK_STREAM, 0);
     memset(&serv_addr, '0', sizeof(serv_addr));
     memset(sendBuff, '0', sizeof(sendBuff)); 
 
-    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_family = PF_UNIX;
     serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     serv_addr.sin_port = htons(5000); 
 
