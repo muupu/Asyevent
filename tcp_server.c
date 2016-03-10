409ded172b1d4962420fe584eb6340fedc7852d5
muupu <once645@gmail.com>
2016-03-10 08:31:06 +0800

    mod tcp_server.c

--- tcp_server.c
+++ tcp_server.c
@@ -11,8 +11,45 @@
 #include <time.h> 
 #include <sys/time.h>
 
+void test_getsockopt()
+{
+    int fds[2];
+  struct ucred cred;
+  socklen_t size;
+
+  if(socketpair(AF_LOCAL, SOCK_STREAM, 0, fds) < 0) {
+    perror("socketpair");
+    return 1;
+  }
+
+  fprintf(stderr, "process:  pid=%i uid=%i gid=%i\n",
+      getpid(), getuid(), getgid());
+
+  size = sizeof(cred);
+  if(getsockopt(fds[1], SOL_SOCKET, SO_PEERCRED, &cred, &size) < 0) {
+    perror("getsockopt/SO_PEERCRED");
+    return 1;
+  }
+  if(size == sizeof(cred)) {
+    fprintf(stderr, "socket:   pid=%i uid=%i gid=%i\n",
+        cred.pid, cred.uid, cred.gid);
+
+    if(getpid() == cred.pid &&
+       getuid() == cred.uid &&
+       getgid() == cred.gid) {
+      printf("IDs are the same, as expected\n");
+      return 0;
+    }
+    else {
+      printf("IDs differ\n");
+    }
+  }
+}
+
 int main(int argc, char *argv[])
 {
+    test_getsockopt();
+    
     int listenfd = 0, connfd = 0;
     struct sockaddr_in serv_addr; 
     struct ucred cred;
